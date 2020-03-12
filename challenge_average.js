e4ba9b82b0cf1e6a070d609e6ae2208aa09ee9fb
EdwinTinoco <jaredlotic@gmail.com>
2020-03-12 11:40:48 -0600

    challenge average in array

--- challenge_average.js
+++ challenge_average.js
@@ -1,13 +1,39 @@
 // Write a program that finds the average 
 
 function average(num_array){
-    counter = 0;
+    let sum = 0;
 
-    for (num of num_array){
-        counter += num;    
+    for (let num of num_array){
+        sum += num;    
     }
     
-    return counter/num_array.length
+    return sum/num_array.length
 }
 
 console.log("The average is: ",average([1,2,3]))
+
+
+// Ryan example
+const getAverage1 = arr => {
+    let sum = 0;
+
+    for (let i=0; i < arr.length; i++){
+        sum += arr[i]; 
+    }
+    
+    return sum/arr.length
+}
+
+console.log("The average is: ",getAverage1([1,2,3]))
+
+
+
+//Jordan example
+const getAverage = arr => {
+    const reducer = (total, currentValue) => total + currentValue;
+    const sum = arr.reduce(reducer);
+
+    return sum / arr.length;    
+}
+
+console.log(getAverage([1,2,3]));
