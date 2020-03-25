8fd36c664004936f1d9c26b92a51f9c8629e6c99
EdwinTinoco <jaredlotic@gmail.com>
2020-03-25 14:31:32 -0600

    challenge pretty price

--- challenge_pretty_price_03252020.js
+++ challenge_pretty_price_03252020.js
@@ -2,7 +2,23 @@
 
 const prettyPrice = (num1, num2) => {
     return Math.floor(num1) + num2; 
-       
+
 }
 
+console.log(prettyPrice(3.50, 0.95));
 console.log(prettyPrice(100, 0.95));
+
+
+
+// Jordan example
+const prettyPrice = (grossPrice, extension) => {
+    if (Number.isInteger(extension)) {
+      extension = extension * 0.01;
+    }
+    return Math.floor(grossPrice) + extension;
+  };
+  
+  prettyPrice(2.2, 0.95); //?
+  prettyPrice(2.2, 95); //?
+  prettyPrice(2.2, 0); //?
+  prettyPrice(2.2, 99); //?
